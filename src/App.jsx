3cbe0cddd37e9310f2eb1b5e2ca789884973a646
Darshana <keerthi.mac@gmail.com>
2022-12-11 23:58:31 +0530

    --3 components added acc to tutorial
    -Changes
        -FeedBar Component Created
        -RightBar Component Created
        -SideBar Component Created
    
        -all components imported to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Feed } from "./components/Feed";
+import { RightBar } from "./components/RightBar";
+import { SideBar } from "./components/Sidebar";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Feed />
+      <RightBar />
+      <SideBar />
+    </>
+  );
 }
 
 export default App;
